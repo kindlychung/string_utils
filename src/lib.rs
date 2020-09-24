a78485dcd9c665682c9b235ceb52b7ee6b50ff5f
Kaiyin Zhong <kindlychung@gmai.com>
2020-09-25 00:38:05 +0200

    fix typo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 /// Returns a &str in which a given suffix is hidden 
 /// See https://stackoverflow.com/a/59330922/562222
-pub fn withotu_suffix<'a>(src: &'a str, suffix: &str) -> &'a str {
+pub fn without_suffix<'a>(src: &'a str, suffix: &str) -> &'a str {
     if src.ends_with(suffix) {
         &src[..src.len() - suffix.len()]
     } else {
@@ -12,9 +12,9 @@ pub fn withotu_suffix<'a>(src: &'a str, suffix: &str) -> &'a str {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn remove_suffix() {
+    fn without_suffix() {
         let a = "xxx.pdf";
-        let b = super::remove_suffix(a, ".pdf");
+        let b = super::without_suffix(a, ".pdf");
         assert_eq!(b, "xxx")
     }
 }
